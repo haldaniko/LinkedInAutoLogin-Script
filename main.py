f2d908ec6072eb3b80c397c858f1f72bd2c8ff63
haldaniko <haldaniko@gmail.com>
2024-11-16 14:27:19 +0100

    remove "--headless" argument from options

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ class LinkedInParser:
         self.password = user_password
 
         options = Options()
-        options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
